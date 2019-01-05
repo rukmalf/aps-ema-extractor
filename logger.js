06468f9a6655397bd88ab931a739d1aeca527027
Rukmal Fernando <rukmal@gmail.com>
2019-01-05 18:30:52 +0530

    add ability to override log level from Heroku config var

--- logger.js
+++ logger.js
@@ -11,6 +11,13 @@ if(config.has('logLevel')) {
 	logLevel = config.get('logLevel');
 }
 
+// Heroku config var can override log level
+if(process.env.LOG_LEVEL) {
+	let logLevelVal = parseInt(process.env.LOG_LEVEL);
+	if(logLevelVal == 1 || logLevelVal == 2) 
+		logLevel = logLevelVal;
+}
+
 function logError(input) {
 	if(logLevel == LOG_LEVEL_NONE) 
 		return;
