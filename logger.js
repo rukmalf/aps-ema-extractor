6d6af5a7150d6b359699d486dbbe05d300e998a4
Rukmal Fernando <rukmal@gmail.com>
2019-01-05 21:36:05 +0530

    improved logging of overrides in logger

--- logger.js
+++ logger.js
@@ -9,13 +9,17 @@ var logLevel = LOG_LEVEL_NONE;
 
 if(config.has('logLevel')) {
 	logLevel = config.get('logLevel');
+	console.log(`LogLevel set to ${logLevel} from config.`);
 }
 
 // Heroku config var can override log level
 if(process.env.LOG_LEVEL) {
+	console.log(`process.env.LOG_LEVEL = ${process.env.LOG_LEVEL}`);
 	let logLevelVal = parseInt(process.env.LOG_LEVEL);
-	if(logLevelVal == 1 || logLevelVal == 2) 
+	if(logLevelVal == 1 || logLevelVal == 2) {
 		logLevel = logLevelVal;
+		console.log(`LogLevel set to ${logLevel} from ENV.`);
+	}
 }
 
 function logError(input) {
